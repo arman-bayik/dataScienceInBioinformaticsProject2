496e84cee07dfc7cddd1769c7187ee14a6c501cd
Arman Bayik <arman.bayik@ufl.edu>
2020-04-24 16:23:48 -0400

    updated scoring to only check for unlinked, adjacent H's, and removed functionality that limited use of an H to 1 H-H bond

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -4,13 +4,10 @@ import random
 
 
 def main():
+    # filename = "SeaCucumberGlobin.txt"
+    # size = 100
+    # population = create_initial_population(filename, size)
     filename = "SeaCucumberGlobin.txt"
-    size = 100
-    population = create_initial_population(filename, size)
-
-
-# method to generate the initial population
-def create_initial_population(filename, size):
     # open and read amino acid sequence from the fasta file
     try:
         input_file = open(filename, "r")
@@ -26,7 +23,29 @@ def create_initial_population(filename, size):
     for sequence_segment in aa_sequence_raw:
         for aa in sequence_segment:
             aa_sequence.append(aa)
+    size = 100
+    population = create_initial_population(aa_sequence, size)
+    scores = []
+    parents = []
+    for i in range(len(population)):
+        scores.append(population[i][2])
+        parents.append(population[i][0])
 
+    breeders = select_parents(scores, parents)
+    children = []
+    for i in range(0, len(population), 1):
+        child = create_child(breeders, aa_sequence)
+        children.append(child)
+
+    print(scores)
+    print(parents)
+    print(children)
+
+    return 0
+
+
+# method to generate the initial population
+def create_initial_population(aa_sequence, size):
     # Create the population of directions; F,L,and R.
     initial_population = []  # variable to store the initial population
     while len(initial_population) < size:
@@ -168,86 +187,145 @@ def score_lattice(d2_lattice):
                   'I': 'H', 'L': 'H', 'K': 'F', 'M': 'H', 'F': 'H',
                   'P': 'H', 'S': 'F', 'T': 'F', 'W': 'H', 'Y': 'H', 'V': 'H'}
     score = 0
-    considered_aas = []
     # traverse d2_lattice matrix
     for r in range(0, len(d2_lattice), 1):
         for c in range(0, len(d2_lattice), 1):
-            is_current_aa_considered = False
             current_aa = d2_lattice[r, c]
             # if the current index contains an amino acid, check for hydrophobia
             if not current_aa[0] == "None":
                 if amino_dict[current_aa[1]] == 'H':
-                    # check if the current amino acid has already been considered for an H-H bond
-                    for i in range(0, len(considered_aas), 1):
-                        # if it has been, set flag
-                        if considered_aas[i] == current_aa[0]:
-                            is_current_aa_considered = True
-                            break
-                    # if the current amino acid has not already been considered for an H-H bond, check for nearby H's
-                    if not is_current_aa_considered:
-                        is_above_aa_considered = False
-                        is_left_aa_considered = False
-                        is_right_aa_considered = False
-                        is_below_aa_considered = False
-                        above_aa = d2_lattice[r-1, c]
-                        left_aa = d2_lattice[r, c-1]
-                        right_aa = d2_lattice[r, c+1]
-                        below_aa = d2_lattice[r+1, c]
-                        # check if nearby amino acids have already been considered for an H-H bond
-                        for i in range(0, len(considered_aas), 1):
-                            # if above amino acid has been considered, set flag
-                            if considered_aas[i] == above_aa[0]:
-                                is_above_aa_considered = True
-                            # if left amino acid has been considered, set flag
-                            if considered_aas[i] == left_aa[0]:
-                                is_left_aa_considered = True
-                            # if right amino acid has been considered, set flag
-                            if considered_aas[i] == right_aa[0]:
-                                is_right_aa_considered = True
-                            # if below amino acid has been considered, set flag
-                            if considered_aas[i] == below_aa[0]:
-                                is_below_aa_considered = True
-                        # if right amino acid hasn't been considered, check if it is an H
-                        if not is_right_aa_considered and not right_aa[1] == "None":
-                            if amino_dict[right_aa[1]] == 'H':
-                                # if right amino acid is an H, check if it's linked to the current amino acid
-                                # if it is an unconsidered H that isn't linked to the current amino acid, it is an H-H connection
-                                if not (current_aa[2] == right_aa[0] or current_aa[3] == right_aa[0]):
-                                    score = score + 1  # increment score
-                                    # add both amino acids to the list of considered amino acids
-                                    considered_aas.append(current_aa[0])
-                                    considered_aas.append(right_aa[0])
-                        # if left amino acid hasn't been considered, check if it is an H
-                        if not is_left_aa_considered and not left_aa[1] == "None":
-                            if amino_dict[left_aa[1]] == 'H':
-                                # if left amino acid is an H, check if it's linked to the current amino acid
-                                # if it is an unconsidered H that isn't linked to the current amino acid, it is an H-H connection
-                                if not (current_aa[2] == left_aa[0] or current_aa[3] == left_aa[0]):
-                                    score = score + 1  # increment score
-                                    # add both amino acids to the list of considered amino acids
-                                    considered_aas.append(current_aa[0])
-                                    considered_aas.append(left_aa[0])
-                        # if above amino acid hasn't been considered, check if it is an H
-                        if not is_above_aa_considered and not above_aa[1] == "None":
-                            if amino_dict[above_aa[1]] == 'H':
-                                # if above amino acid is an H, check if it's linked to the current amino acid
-                                # if it is an unconsidered H that isn't linked to the current amino acid, it is an H-H connection
-                                if not (current_aa[2] == above_aa[0] or current_aa[3] == above_aa[0]):
-                                    score = score + 1  # increment score
-                                    # add both amino acids to the list of considered amino acids
-                                    considered_aas.append(current_aa[0])
-                                    considered_aas.append(above_aa[0])
-                        # if below amino acid hasn't been considered, check if it is an H
-                        if not is_below_aa_considered and not below_aa[1] == "None":
-                            if amino_dict[below_aa[1]] == 'H':
-                                # if below amino acid is an H, check if it's linked to the current amino acid
-                                # if it is an unconsidered H that isn't linked to the current amino acid, it is an H-H connection
-                                if not (current_aa[2] == below_aa[0] or current_aa[3] == below_aa[0]):
-                                    score = score + 1  # increment score
-                                    # add both amino acids to the list of considered amino acids
-                                    considered_aas.append(current_aa[0])
-                                    considered_aas.append(below_aa[0])
+                    # check for nearby H's
+                    above_aa = d2_lattice[r-1, c]
+                    left_aa = d2_lattice[r, c-1]
+                    right_aa = d2_lattice[r, c+1]
+                    below_aa = d2_lattice[r+1, c]
+                    # check if right amino acid is an H
+                    if not right_aa[1] == "None":
+                        if amino_dict[right_aa[1]] == 'H':
+                            # if right amino acid is an H, check if it's linked to the current amino acid
+                            # if it is an H that isn't linked to the current amino acid, it is an H-H connection
+                            if not (current_aa[2] == right_aa[0] or current_aa[3] == right_aa[0]):
+                                score = score + 1  # increment score
+                    # check if left amino acid is an H
+                    if not left_aa[1] == "None":
+                        if amino_dict[left_aa[1]] == 'H':
+                            # if left amino acid is an H, check if it's linked to the current amino acid
+                            # if it is an H that isn't linked to the current amino acid, it is an H-H connection
+                            if not (current_aa[2] == left_aa[0] or current_aa[3] == left_aa[0]):
+                                score = score + 1  # increment score
+                    # check if above amino acid is an H
+                    if not above_aa[1] == "None":
+                        if amino_dict[above_aa[1]] == 'H':
+                            # if above amino acid is an H, check if it's linked to the current amino acid
+                            # if it is an H that isn't linked to the current amino acid, it is an H-H connection
+                            if not (current_aa[2] == above_aa[0] or current_aa[3] == above_aa[0]):
+                                score = score + 1  # increment score
+                    # check if below amino acid is an H
+                    if not below_aa[1] == "None":
+                        if amino_dict[below_aa[1]] == 'H':
+                            # if below amino acid is an H, check if it's linked to the current amino acid
+                            # if it is an H that isn't linked to the current amino acid, it is an H-H connection
+                            if not (current_aa[2] == below_aa[0] or current_aa[3] == below_aa[0]):
+                                score = score + 1  # increment score
     return score
+
+
+def select_parents(scores, parents):
+    # parents is a list that stores lists of parent path sequences
+    # scores is a list that stores the score of each parent.
+
+    # Normalize the scores and place them in a list; normalized scores
+    normalized_scores = []
+    sum_scores = sum(scores)
+    for i in range(len(scores)):
+        normalized_scores.append(scores[i] / sum_scores)
+
+    # We need to create a population pool based off of the normalized scores. A parents score is proportional to the
+    # number of copies of itself in the mating pool In this example parent1 has a 15% probability of being a parent.
+    # If we initialize the mating pool to 100. 15 of the parents will be parent1
+    pool_size = 100
+    pool = []
+
+    for i in range(len(normalized_scores)):
+        number_in_pop = int(normalized_scores[i] * pool_size)
+        for n in range(number_in_pop):
+            pool.append(parents[i])
+
+    # Now the pool contains the parents in their corresponding proportions relative to the pool size
+    # Randomize the pool of parents and then randomly select two parents.
+    random.shuffle(pool)
+    parent_one_index = 1
+    parent_two_index = 1
+
+    # This code allows us to have two unique parents
+    while pool[parent_one_index] == pool[parent_two_index]:
+        parent_one_index = random.randint(0, len(pool) - 1)
+        parent_two_index = random.randint(0, len(pool) - 1)
+
+    selected_parents = [pool[parent_one_index], pool[parent_two_index]]
+    return selected_parents
+
+
+# After selecting the parents we create a child using these two parents
+def create_child(selected_parents, aa_sequence):
+    # spawn is the child of the selected_parents.
+    spawn = []
+
+    # Use the 50-50 method to create a new child
+    midpoint = int(len(selected_parents[0]) / 2)
+    first_half = selected_parents[0][0:midpoint]
+    second_half = selected_parents[1][midpoint:len(selected_parents[1])]
+
+    child_traversal_sequence = first_half + second_half
+    child = mutate(child_traversal_sequence, aa_sequence)
+    return child
+
+
+# We have to have a function that mutates the children to introduce variation into the population.
+def mutate(child_traversal_sequence, aa_sequence):
+    # We have to have a mutation rate as well.
+    # 1% mutation rate
+    mutation_rate = 1
+    bump = True
+    size = len(child_traversal_sequence)
+    child_d2_lattice = numpy.ndarray([2 * size + 2, 2 * size + 2], dtype=object)
+    for r in range(0, len(child_d2_lattice), 1):
+        for c in range(0, len(child_d2_lattice[r]), 1):
+            # format is: [index, amino acid, previous index, next index]
+            child_d2_lattice[r, c] = ["None", "None", "None", "None"]
+    while bump:
+        for i in range(len(child_traversal_sequence)):
+            random_number = random.randint(0, 100)
+            letter = ['F', 'L', 'R']
+            random_letter_index = random.randint(0, 2)
+            if random_number <= mutation_rate:
+                child_traversal_sequence[i] = letter[random_letter_index]
+
+        r_index = int(size)
+        c_index = int(size)  # indices in the d2_lattice matrix
+        orientation = 'E'  # current orientation in units of cardinal direction
+        for i in range(0, len(child_traversal_sequence), 1):
+            if i == 0:
+                child_d2_lattice[r_index, c_index] = [i, aa_sequence[i], "None", i+1]
+            elif i == len(aa_sequence)-1:
+                child_d2_lattice[r_index, c_index] = [i, aa_sequence[i], i-1, "None"]
+            else:
+                child_d2_lattice[r_index, c_index] = [i, aa_sequence[i], i-1, i+1]
+
+            current_traversal = child_traversal_sequence[i]
+            current_traversal_info = get_traversal_info(current_traversal, orientation)
+            new_r_index = r_index + current_traversal_info[0]
+            new_c_index = c_index + current_traversal_info[1]
+            if child_d2_lattice[new_r_index, new_c_index][0] == "None":
+                bump = False
+                orientation = current_traversal_info[2]
+                r_index = new_r_index
+                c_index = new_c_index
+            else:
+                bump = True
+                break
+    print("Mutation complete")
+    return [child_traversal_sequence, child_d2_lattice, score_lattice(child_d2_lattice)]
 
 
 if __name__ == '__main__':
